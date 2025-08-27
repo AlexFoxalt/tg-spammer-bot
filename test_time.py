efd17f23f277bd1ec44fc2c11fe8374d680d5493
Alex Chernyshov <alexfoxalt@gmail.com>
2025-08-27 11:28:56 +0300

    Replace print statements with logger for time info

--- test_time.py
+++ test_time.py
@@ -1,3 +1,8 @@
-from datetime import datetime
+from logger import get_logger
 
-print(datetime.now())
+logger = get_logger()
+
+logger.info("utcnow: %s", dt.datetime.utcnow().isoformat() + "Z")
+logger.info("local (naive) now: %s", dt.datetime.now().isoformat())
+logger.info("zone-aware now: %s", dt.datetime.now(TZ).isoformat())
+logger.info("time.tzname: %s", time.tzname)
